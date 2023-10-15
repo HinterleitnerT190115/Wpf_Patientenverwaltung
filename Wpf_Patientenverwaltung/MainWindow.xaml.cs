7c779b1d7af2edb019d2a0af2692005fd7511596
HinterleitnerT190115 <106739611+HinterleitnerT190115@users.noreply.github.com>
2023-10-15 21:55:56 +0200

    Added exercise extension to the project, including some UI tweaks

--- Wpf_Patientenverwaltung/MainWindow.xaml.cs
+++ Wpf_Patientenverwaltung/MainWindow.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Xml.XPath;
 
 namespace Wpf_Patientenverwaltung
 {
@@ -21,6 +26,17 @@ namespace Wpf_Patientenverwaltung
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             listViewPatients.ItemsSource = patientList;
+            btnAddDisease.IsEnabled = false;
+            btnDeleteSelectedPatient.IsEnabled = false;
+
+            //TEMP for Testing
+            textBoxFirstname.Text = "Max";
+            textBoxLastname.Text = "Muster";
+            datePickerBirthday.SelectedDate = DateTime.Now;
+            radioButtonMale.IsChecked = false;
+            radioButtonFemale.IsChecked = true;
+            checkBoxBedWetter.IsChecked = true;
+            comboboxDiseases.SelectedIndex = 0;
         }
 
         private void miOpen_Click(object sender, RoutedEventArgs e)
@@ -33,6 +49,7 @@ namespace Wpf_Patientenverwaltung
             if(ofd.ShowDialog() != true) { return; }
 
             patientList.Clear();
+            stackPanelPatients.Children.Clear();
 
             using (var sr = new StreamReader(ofd.FileName))
             {
@@ -43,6 +60,7 @@ namespace Wpf_Patientenverwaltung
                     if (Patient.TryParse(line, out p))
                     {
                         patientList.Add(p);
+                        AddItemToStackPanel(p);
                     }
                 }
             }
@@ -124,12 +142,70 @@ namespace Wpf_Patientenverwaltung
                 IsBedWetter = (bool)isBedWetter,
             };
 
+
+
             patientList.Add(p);
+            AddItemToStackPanel(p);
         }
 
-        private void btnDeletePatient_Click(object sender, RoutedEventArgs e)
+        private void AddItemToStackPanel(Patient p)
         {
-            patientList.Remove((Patient)listViewPatients.SelectedItem);
+            string name = p.Firstname + " " + p.Lastname;
+            stackPanelPatients.Children.Add(new Label()
+            {
+                Content = name,
+                Background = System.Windows.Media.Brushes.Red,
+                Width = 150,
+                Height = 30,
+                Margin = new Thickness(5)
+            });
+        }
+
+        private void btnAddDisease_Click(object sender, RoutedEventArgs e)
+        {
+            string disease = (string)((System.Windows.Controls.ComboBoxItem)comboboxDiseases.SelectedValue).Content;
+            patientList[listViewPatients.SelectedIndex].Diseases.Add(disease);
+
+            //Otherwise diseases will not be updated in the ListView
+            CollectionViewSource.GetDefaultView(listViewPatients.ItemsSource).Refresh();
+        }
+
+        private void btnDeleteSelectedPatient_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedPatient = (Patient)listViewPatients.SelectedItem;
+
+            patientList.Remove(selectedPatient);
+            for (int i = 0; i < stackPanelPatients.Children.Count; i++)
+            {
+                Label label = (Label)stackPanelPatients.Children[i];
+
+                string name = selectedPatient.Firstname + " " + selectedPatient.Lastname;
+
+                if(label.Content.ToString().Equals(name))
+                {
+                    stackPanelPatients.Children.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        private void btnDeleteAllPatients_Click(object sender, RoutedEventArgs e)
+        {
+            patientList.Clear();
+            stackPanelPatients.Children.Clear();
+        }
+
+        private void listViewPatients_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count > 0)
+            {
+                btnAddDisease.IsEnabled = true;
+                btnDeleteSelectedPatient.IsEnabled = true;
+            } else
+            {
+                btnAddDisease.IsEnabled = false;
+                btnDeleteSelectedPatient.IsEnabled = false;
+            }
         }
     }
 }
